96841c0a8ee143107614162b35866ea04c2a4bb9
Rafael Franco <rafaelfariafranco30@gmail.com>
2024-09-05 17:51:14 -0300

    Add files via upload

---
+++ Java/Prj8 - Conta Bancária/src/entities/Conta.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Conta {
+    private int id;
+    private String cpf;
+    private float saldo;
+
+
+
+  public void setSaldo(float saldo) {
+      this.saldo = saldo;
+  }
+
+}
